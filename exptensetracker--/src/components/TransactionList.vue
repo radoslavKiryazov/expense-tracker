5b005a00571bd79aac6af924f90758e2f43a86a6
Radoslav Kiryazov <radokiryazov11230@gmail.com>
2024-09-25 18:47:54 +0200

    defining props

---
+++ exptensetracker--/src/components/TransactionList.vue
@@ -0,0 +1,29 @@
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li
+      v-for="transaction in transactions"
+      :key="transaction.id"
+      :class="transaction.amount > 0 ? 'plus' : 'minus'"
+    >
+      {{ transaction.text }} <span>${{ transaction.amount }}</span
+      ><button class="delete-btn">x</button>
+    </li>
+    <!-- <li class="minus">
+          Cash <span>-$400</span><button class="delete-btn">x</button>
+        </li>
+        <li class="plus">
+          Cash <span>+$400</span><button class="delete-btn">x</button>
+        </li> -->
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
